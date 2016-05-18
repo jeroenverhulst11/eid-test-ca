c59ff4ee42ac6faee5325bdb372a297ce1148984
Frederikus <Frederik.v*******@f*****.be>
2016-05-18 11:30:00 +0200

    added the signed 32bit binary fromcard.exe

--- scripts/windows/sign.bat
+++ scripts/windows/sign.bat
@@ -11,6 +11,9 @@ set FROMCARDINSTALLPATH=%~dp0..\..\Release
 "%SIGNTOOL_PATH%\signtool" sign /fd SHA1 /s MY  /n "Fedict" /sha1 "2259EF223A51E91964D7F4695706091194E018BB" /t http://timestamp.verisign.com/scripts/timestamp.dll /v "%FROMCARDINSTALLPATH%\FromCard.exe"
 "%SIGNTOOL_PATH%\signtool" sign /as /fd SHA256 /s MY /n "Fedict" /sha1 "2259EF223A51E91964D7F4695706091194E018BB" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 /v "%FROMCARDINSTALLPATH%\FromCard.exe"
 
+del %~dp0\FromCard.exe
+copy "%FROMCARDINSTALLPATH%\FromCard.exe" %~dp0\FromCard.exe
+
 @if "%ERRORLEVEL%" == "1" goto signtool_failed
 
 goto end_resetpath
